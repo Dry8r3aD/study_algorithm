65545bd4d914702d6e68ffc9ee805177bc6f6404
Gunyoung Yoon <me@dry8r3ad.com>
2018-02-18 21:14:30 +0900

    Tools added
    - Script for creating problem dir

---
+++ tools/make_problem_dir.sh
@@ -0,0 +1,43 @@
+REPO_NAME="study_fuck"
+ROOT_DIR=$(find / -name "$REPO_NAME")
+MAKE_FILE="C_sample_makefile"
+
+echo "Script for creating new problem directory"
+
+echo "Enter problem number"
+read prob_num
+
+echo "Enter which language to use (Ex. C, Python, Etc)"
+read lang
+
+if [ -d "$ROOT_DIR/$prob_num" ]
+then
+	echo "Problem directory exsits"
+else
+	echo "Creating problem directory"
+	mkdir $ROOT_DIR/$prob_num
+fi
+
+if [ -d "$ROOT_DIR/$prob_num/$lang" ]
+then
+	echo "Language directory excists"
+else
+	echo "Creating language directory"
+	mkdir $ROOT_DIR/$prob_num/$lang
+fi
+
+echo $lang
+if [ "$lang" = "C" ]
+then
+	if [ -f $ROOT_DIR/$prob_num/$lang/Makefile ]
+	then
+		echo "Makefile excists for C language"
+	else
+		echo "Creating default Makefile"
+		cp $MAKE_FILE "$ROOT_DIR/$prob_num/$lang/Makefile"
+	fi
+else
+	echo "Sample/Default makefile does not exsits for this language $lang"
+fi
+
+echo "Creating problem directory done"
