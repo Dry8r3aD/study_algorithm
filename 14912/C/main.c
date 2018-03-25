adf25b4d9f2aa12337c6ab5c912c9cb02a7cbdf7
Gunyoung Yoon <me@dry8r3ad.com>
2018-03-25 23:47:56 +0900

    Algorithm Study :: Problem 14912
    - Fixed bug
      - Started counting numbers from 0 instead of 1

--- 14912/C/main.c
+++ 14912/C/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define INT_MAX_DIGIT 10
+#define MAX_NUMBER_DIGIT 6
 #define ASCII_CORRECTION 48
 
 int main( void )
@@ -8,15 +8,15 @@ int main( void )
     int max_num = 0;
     int freq_counting = 0;
     int freq_number = 0;
-    char num_ch[INT_MAX_DIGIT + 1] = { 0, };
+    char num_ch[MAX_NUMBER_DIGIT + 1] = { 0, };
 
     scanf( "%d %d", &max_num, &freq_number );
 
-    for ( int i = 0; i <= max_num; i++ )
+    for ( int i = 1; i <= max_num; i++ )
     {
         sprintf( num_ch, "%d", i );
 
-        for ( int j = 0; j <= INT_MAX_DIGIT; j++ )
+        for ( int j = 0; j <= MAX_NUMBER_DIGIT; j++ )
         {
             if ( num_ch[j] == '\0' )
             {
