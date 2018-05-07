46494f0888cc2000af4d9e97de9d2acf0e0281c9
Gunyoung Yoon <me@dry8r3ad.com>
2018-05-07 21:29:21 +0900

    Changed directory structure
    - Categorized the problem sources
      - Baekjoon, Codility, Daily-Programming
      - Modifited tool/script due to directory changes
    - Created "working" directory in local repo, and addes to .gitignore
      - Not finished problems will be located here

--- tools/make_problem_dir.sh
+++ tools/make_problem_dir.sh
@@ -4,37 +4,54 @@ MAKE_FILE="C_sample_makefile"
 
 echo "Script for creating new problem directory"
 
+echo "Enter problem source ( Ex. baekjoon, codility, daily, etc )"
+read prob_source
+
 echo "Enter problem number"
 read prob_num
 
 echo "Enter which language to use (Ex. C, Python, Etc)"
 read lang
 
-if [ -d "$ROOT_DIR/$prob_num" ]
+if [ "$prob_source" = "baekjoon" ]
+then
+	PROB_DIR=$ROOT_DIR/Baekjoon
+elif [ "$prob_source" = "codility" ]
+then
+	PROB_DIR=$ROOT_DIR/Codility
+elif [ "$prob_source" = "daily" ]
+then
+	PROB_DIR=$ROOT_DIR/Daily-Programming
+else
+	echo "Not Pre-defined problem source"
+	exit
+fi
+
+if [ -d "$PROB_DIR/$prob_num" ]
 then
 	echo "Problem directory exsits"
 else
 	echo "Creating problem directory"
-	mkdir $ROOT_DIR/$prob_num
+	mkdir $PROB_DIR/$prob_num
 fi
 
-if [ -d "$ROOT_DIR/$prob_num/$lang" ]
+if [ -d "$PROB_DIR/$prob_num/$lang" ]
 then
 	echo "Language directory excists"
 else
 	echo "Creating language directory"
-	mkdir $ROOT_DIR/$prob_num/$lang
+	mkdir $PROB_DIR/$prob_num/$lang
 fi
 
 echo $lang
 if [ "$lang" = "C" ]
 then
-	if [ -f $ROOT_DIR/$prob_num/$lang/Makefile ]
+	if [ -f $PROB_DIR/$prob_num/$lang/Makefile ]
 	then
 		echo "Makefile excists for C language"
 	else
 		echo "Creating default Makefile"
-		cp $MAKE_FILE "$ROOT_DIR/$prob_num/$lang/Makefile"
+		cp $MAKE_FILE "$PROB_DIR/$prob_num/$lang/Makefile"
 	fi
 else
 	echo "Sample/Default makefile does not exsits for this language $lang"
