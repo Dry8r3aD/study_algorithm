9c1af1249714f2dcc556a982ec18f9266fb898f0
Gunyoung Yoon <me@dry8r3ad.com>
2019-08-08 22:59:37 +0900

    Algorithm Study - Baekjoon 9498

---
+++ Baekjoon/9498/C/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main ( void )
+{
+	int score = 0;
+
+	scanf( "%d", &score );
+
+	if ( 90 <= score && score <= 100 )
+	{
+		printf( "A\n" );
+	}
+	else if ( 80 <= score && score <= 89)
+	{
+		printf( "B\n" );
+	}
+	else if ( 70 <= score && score <= 79 )
+	{
+		printf( "C\n" );
+	}
+	else if ( 60 <= score && score <= 69 )
+	{
+		printf( "D\n" );
+	}
+	else
+	{
+		printf( "F\n" );
+	}
+
+	return 0;
+}
